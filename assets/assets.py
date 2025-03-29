0d829e445a52af8df5ec3bda157644db32d60471
6reenhorn <coding.cyrilj777@gmail.com>
2025-03-29 22:10:20 +0800

    style(assets.py): Window icon update

--- assets/assets.py
+++ assets/assets.py
@@ -2,4 +2,4 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-WINDOW_ICON = os.path.join(BASE_DIR, "images", "data-processing.png")
+WINDOW_ICON = os.path.join(BASE_DIR, "images", "windowIcon_2.png")
