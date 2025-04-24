f93cc707ec6454f7ed40c94522d36ee6fcca7e69
6reenhorn <coding.cyrilj777@gmail.com>
2025-04-24 23:17:59 +0800

    Added new image asset

--- assets/assets.py
+++ assets/assets.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-WINDOW_ICON = os.path.join(BASE_DIR, "images", "windowIcon_2.png")
+WINDOW_ICON = os.path.join(BASE_DIR, "images", "windowIcon_2.ico")
 LOADING_ICON = os.path.join(BASE_DIR, "images", "loadingIcon_2.gif")
